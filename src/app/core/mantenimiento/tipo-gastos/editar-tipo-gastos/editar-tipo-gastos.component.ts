27ffb47bbd44e1851837959a18981d89e6bbd25b
Cristhofer Salcedo <emoword123@gmail.com>
2023-10-13 13:51:54 -0500

    editar-tipo-gastos

--- src/app/core/mantenimiento/tipo-gastos/editar-tipo-gastos/editar-tipo-gastos.component.ts
+++ src/app/core/mantenimiento/tipo-gastos/editar-tipo-gastos/editar-tipo-gastos.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { ITipoGasto } from 'src/app/shared/models/tipogastos';
+import { routes } from 'src/app/shared/routes/routes';
+import { TipoGastosService } from 'src/app/shared/services/tipo-gastos.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-editar-tipo-gastos',
   templateUrl: './editar-tipo-gastos.component.html',
   styleUrls: ['./editar-tipo-gastos.component.scss']
 })
-export class EditarTipoGastosComponent {
+export class EditarTipoGastosComponent implements OnInit {
+  tipoGastoSeleccionado: ITipoGasto | null = null;
+  public routes = routes;
+  form: FormGroup;
+  public mostrarErrores = false;
 
+  constructor(public bsModalRef: BsModalRef, private tipoGastosService: TipoGastosService, public fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      estado: ['Activo', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    if (this.tipoGastoSeleccionado) {
+      this.form.patchValue({
+        nombre: this.tipoGastoSeleccionado.nombre,
+        estado: this.tipoGastoSeleccionado.estado,
+      });
+    }
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.invalid && control?.touched;
+  }
+
+  isRequerido(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.errors && control.errors['required'];
+  }
+
+  Cancelar() {
+    this.bsModalRef.hide();
+  }
+
+  guardarTipoGasto() {
+    if (!this.tipoGastoSeleccionado || this.form.invalid) {
+      this.mostrarErrores = true;
+      return;
+    }
+
+    const tipoGastoActualizado: ITipoGasto = {
+      tipoGastoId: this.tipoGastoSeleccionado.tipoGastoId,
+      nombre: this.form.value.nombre,
+      estado: this.form.value.estado,
+    };
+
+    this.tipoGastosService.actualizarTipoGasto(tipoGastoActualizado).subscribe(
+      (response) => {
+        if (response.isSuccess) {
+          Swal.fire(response.message, '', 'success');
+          this.bsModalRef.hide();
+        } else {
+          console.error(response.message);
+        }
+      },
+      (error) => {
+        console.error(error);
+      });
+  }
 }
