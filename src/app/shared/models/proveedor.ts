f45b15bf46ea784c1af2dcf2cf9040cb1cd5c5c0
HorDdaTh <nicolasalf147@gmail.com>
2023-10-13 09:03:24 -0500

    creacion Proveedor

---
+++ src/app/shared/models/proveedor.ts
@@ -0,0 +1,23 @@
+export class proveedor {
+    clinicaId = 'D30C2D1E-E883-4B2D-818A-6813E15046E6';
+    usuarioId = "";
+    ruc = "";
+    nombre = "";
+    direccion =  "";
+    telefono ="";
+    contacto =  "";
+    correo=  "";
+  }
+  export interface DataProducto {
+    totalData: number;
+    data: Iproveedor[];
+  }
+  export interface Iproveedor {
+    ruc : string;
+    nombre : string;
+    direccion : string;
+    telefono : string;
+    contacto : string;
+    correo: string;
+
+  }
