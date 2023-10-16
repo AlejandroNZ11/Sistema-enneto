bfc391bcb3c890f807621e18335b73a779e0aab3
Alejandro Nuñez <schnaidernunez@gmail.com>
2023-10-16 15:49:24 -0500

    implementacion de categoria-op

---
+++ src/app/core/operaciones/categoria/agregar-categoria/agregar-categoria.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { DataCategoria, Icategoria, categoria } from 'src/app/shared/models/categoria-op';
+import { routes } from 'src/app/shared/routes/routes';
+import { CategoriaOpService } from 'src/app/shared/services/categoria-op.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-agregar-categoria',
+  templateUrl: './agregar-categoria.component.html',
+  styleUrls: ['./agregar-categoria.component.scss']
+})
+export class AgregarCategoriaComponent implements OnInit{
+
+  Categoria: categoria = new categoria();
+  public routes = routes;
+  form!: FormGroup;
+  public mostrarErrores = false;
+  ngOnInit(): void { }
+
+  constructor(public bsModalRef: BsModalRef, private service: CategoriaOpService,
+    public fb: FormBuilder,) {
+    this.form = this.fb.group({
+      descripcion: ['', Validators.required],
+    });
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.invalid && control?.touched;
+  }
+  isRequerido(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.errors && control.errors['required'];
+  }
+  Cancelar() {
+    this.bsModalRef.hide()
+  }
+  isTouched() {
+    Object.values(this.form.controls).forEach((control) => {
+      control.markAsTouched();
+    });
+  }
+  crearCategoria() {
+    if (this.form.invalid) {
+      this.isTouched()      
+      return;
+    }
+    this.Categoria.nombre = this.form.get("descripcion")?.value;
+    console.log(this.Categoria);
+    this.service.crearCategoria(this.Categoria).subscribe(
+      (response)=>{
+        if(response.isSuccess){
+          Swal.fire(response.message, '', 'success');
+          this.bsModalRef.hide();
+        }else{
+          console.error(response.message);
+        }
+      },
+      (error)=>{
+        console.error(error);
+      });
+  }
+}
+
+
