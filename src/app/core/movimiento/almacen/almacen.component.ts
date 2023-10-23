d5e36638b08240dd9d6ad7d712bc47ca83e80776
HorDdaTh <nicolasalf147@gmail.com>
2023-10-23 14:49:50 -0500

    add | almacen cambios

--- src/app/core/movimiento/almacen/almacen.component.ts
+++ src/app/core/movimiento/almacen/almacen.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { routes } from 'src/app/shared/routes/routes';
 
 @Component({
   selector: 'app-almacen',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./almacen.component.scss']
 })
 export class AlmacenComponent {
+  public routes = routes;
+
+  public showFilter = false;
+  public searchDataValue = '';
+  public lastIndex = 0;
+  public pageSize = 10;
+  public totalData = 0;
+  public skip = 0;
+  public limit: number = this.pageSize;
+  public pageIndex = 0;
+  public serialNumberArray: Array<number> = [];
+  public currentPage = 1;
+  public pageNumberArray: Array<number> = [];
+
+  public totalPages = 0;
+  bsModalRef?: BsModalRef;
+  constructor(private modalService: BsModalService) {
+  }
 
 }
