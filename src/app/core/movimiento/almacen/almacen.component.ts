2aaa3836b31b23111d5ae282be7de5490323440b
HorDdaTh <nicolasalf147@gmail.com>
2023-10-23 14:59:57 -0500

    delete  almacen |add diagnostico

--- src/app/core/movimiento/almacen/almacen.component.ts
+++ src/app/core/movimiento/almacen/almacen.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { routes } from 'src/app/shared/routes/routes';
+
 
 @Component({
   selector: 'app-almacen',
@@ -8,23 +7,5 @@ import { routes } from 'src/app/shared/routes/routes';
   styleUrls: ['./almacen.component.scss']
 })
 export class AlmacenComponent {
-  public routes = routes;
-
-  public showFilter = false;
-  public searchDataValue = '';
-  public lastIndex = 0;
-  public pageSize = 10;
-  public totalData = 0;
-  public skip = 0;
-  public limit: number = this.pageSize;
-  public pageIndex = 0;
-  public serialNumberArray: Array<number> = [];
-  public currentPage = 1;
-  public pageNumberArray: Array<number> = [];
-
-  public totalPages = 0;
-  bsModalRef?: BsModalRef;
-  constructor(private modalService: BsModalService) {
-  }
-
+  
 }
