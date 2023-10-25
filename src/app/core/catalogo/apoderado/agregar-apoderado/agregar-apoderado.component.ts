e9b3a58b715a2cf88720019897903c6a82e1fd37
Cristhofer Salcedo <emoword123@gmail.com>
2023-10-25 14:42:26 -0500

    Update | agregar-apoderado

--- src/app/core/catalogo/apoderado/agregar-apoderado/agregar-apoderado.component.ts
+++ src/app/core/catalogo/apoderado/agregar-apoderado/agregar-apoderado.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { ApoderadoService } from 'src/app/shared/services/apoderado.service';
+import { Apoderado } from 'src/app/shared/models/apoderado';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-agregar-apoderado',
@@ -7,4 +12,64 @@ import { Component } from '@angular/core';
 })
 export class AgregarApoderadoComponent {
 
+  public form!: FormGroup;
+  public mostrarErrores = false;
+
+  constructor(public bsModalRef: BsModalRef, private apoderadoService: ApoderadoService,
+    public fb: FormBuilder) {
+    this.form = this.fb.group({
+      nombre: ['', Validators.required],
+      tipoDocumento: ['', Validators.required],
+      documento: ['', Validators.required],
+      direccion: [''],
+      telefono: [''],
+    });
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.invalid && control?.touched;
+  }
+
+  isRequerido(controlName: string) {
+    const control = this.form.get(controlName);
+    return control?.errors && control.errors['required'];
+  }
+
+  Cancelar() {
+    this.bsModalRef.hide()
+  }
+
+  isTouched() {
+    Object.values(this.form.controls).forEach((control) => {
+      control.markAsTouched();
+    });
+  }
+
+  crearApoderado() {
+    if (this.form.invalid) {
+        this.isTouched()      
+        return;
+    }
+    const nuevoApoderado = new Apoderado();
+    nuevoApoderado.nombre = this.form.get("nombre")?.value;
+    nuevoApoderado.tipoDocumento = this.form.get("tipoDocumento")?.value;
+    nuevoApoderado.documento = this.form.get("documento")?.value;
+    nuevoApoderado.direccion = this.form.get("direccion")?.value;
+    nuevoApoderado.telefono = this.form.get("telefono")?.value;
+
+    this.apoderadoService.crearApoderado(nuevoApoderado).subscribe(
+        (response) => {
+            if (response.isSuccess) {
+                Swal.fire(response.message, '', 'success');
+                this.bsModalRef.hide();
+            } else {
+                console.error(response.message);
+            }
+        },
+        (error) => {
+            console.error(error);
+        }
+    );
+  }
 }
