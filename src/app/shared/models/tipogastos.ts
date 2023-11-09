8509c8caa7ad4a40fa9ea2a8eeee8a42ad661e72
Cristhofer Salcedo <emoword123@gmail.com>
2023-11-09 14:44:26 -0500

    Update | tipo-gastos.service.ts

--- src/app/shared/models/tipogastos.ts
+++ src/app/shared/models/tipogastos.ts
@@ -1,4 +1,4 @@
-export class tipoGasto {
+export class TipoGasto {
     clinicaId = 'D30C2D1E-E883-4B2D-818A-6813E15046E6';
     usuarioId = "";
     nombre = "";
@@ -13,4 +13,8 @@ export interface ITipoGasto {
     tipoGastoId: string;
     nombre: string;
     estado: string;
+}
+
+export class tipoGastosResponse {
+    nombre = "";
 }
