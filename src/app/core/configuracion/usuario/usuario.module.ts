736432b30fe502bfceea83a73347cae9dc0b4d90
Cristhofer Salcedo <emoword123@gmail.com>
2023-11-14 15:26:52 -0500

    Update | usuario
    HTML y TS

--- src/app/core/configuracion/usuario/usuario.module.ts
+++ src/app/core/configuracion/usuario/usuario.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { UsuarioComponent } from './usuario.component';
 import { UsuarioRoutingModule } from './usuario-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AgregarUsuarioComponent } from './agregar-usuario/agregar-usuario.component';
+import { EditarUsuarioComponent } from './editar-usuario/editar-usuario.component';
 
 
 
 @NgModule({
   declarations: [
     UsuarioComponent,
+    AgregarUsuarioComponent,
+    EditarUsuarioComponent,
   ],
   imports: [
     CommonModule,
