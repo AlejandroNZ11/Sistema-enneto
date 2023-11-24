e699476bdfcd9d84aed1e4840084ea87dbe16d23
HorDdaTh <nicolasalf147@gmail.com>
2023-11-24 14:09:24 -0500

    add| editar proveedor

--- src/app/core/operaciones/proveedor/proveedor.module.ts
+++ src/app/core/operaciones/proveedor/proveedor.module.ts
@@ -4,13 +4,15 @@ import { ProveedorComponent } from './proveedor.component';
 import { ProveedorRoutingModule } from './proveedor-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AgregarProveedorComponent } from './agregar-proveedor/agregar-proveedor.component';
+import { EditarProveedorComponent } from './editar-proveedor/editar-proveedor.component';
 
 
 
 @NgModule({
   declarations: [
     ProveedorComponent,
-    AgregarProveedorComponent
+    AgregarProveedorComponent,
+    EditarProveedorComponent
   ],
   imports: [
     CommonModule,
