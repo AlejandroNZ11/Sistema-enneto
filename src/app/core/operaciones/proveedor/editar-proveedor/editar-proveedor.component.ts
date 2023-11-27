cf35f566ef844e0ba3830d6f310b147a3da56016
HorDdaTh <nicolasalf147@gmail.com>
2023-11-27 12:49:43 -0500

    update | editar proveedor validators

--- src/app/core/operaciones/proveedor/editar-proveedor/editar-proveedor.component.ts
+++ src/app/core/operaciones/proveedor/editar-proveedor/editar-proveedor.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { Iproveedor } from 'src/app/shared/models/proveedor';
+import { routes } from 'src/app/shared/routes/routes';
+import { ProveedorService } from 'src/app/shared/services/proveedor.service';
 
 @Component({
   selector: 'app-editar-proveedor',
@@ -6,5 +11,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-proveedor.component.scss']
 })
 export class EditarProveedorComponent {
-
+  proveedor!: Iproveedor;
+  proveedorSeleccionado: any;
+  public routes = routes;
+  form: FormGroup;
+  public mostrarErrores = false;
+  constructor(public bsModalRef: BsModalRef, private ProveedorService: ProveedorService, public fb: FormBuilder) {
+    this.form = this.fb.group({
+      ruc: ['', Validators.required],
+      nombre: ['', Validators.required],
+      direccion: ['', Validators.required],
+      telefono: ['', Validators.required],
+      contacto: ['', Validators.required],
+      correo: ['', Validators.required],
+      estado: ['Activo', Validators.required],
+    });
+  }
 }
