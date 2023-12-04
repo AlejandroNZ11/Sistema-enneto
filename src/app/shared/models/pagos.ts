9f137c475f738417e3e1fadc55feefebc500e92e
JASIIEL <130499497+JASIIEL@users.noreply.github.com>
2023-12-04 15:55:49 -0500

    uptade-agregar pago

--- src/app/shared/models/pagos.ts
+++ src/app/shared/models/pagos.ts
@@ -1,14 +1,13 @@
 export class PagosRequest {
-    TransaccionId = '';
-    NumeroPago!: number;
-    EstadoPago!: number;
-    FechaRegistro !: Date;
-    FechaVencimiento!: Date;
-    Monto!: number;
-    IdUsuario !: number;
-    TratamientoId = '';
-    ClinicaId = 'D30C2D1E-E883-4B2D-818A-6813E15046E6';
-    UsuarioId = '';
+    transaccionId = '3fa85f64-5717-4562-b3fc-2c963f66afa6';
+    numeroPago!: number;
+    estadoPago!: number;
+    fechaRegistro !: Date;
+    fechaVencimiento!: Date;
+    monto!: number;
+    idUsuario !: number;
+    clinicaId = 'D30C2D1E-E883-4B2D-818A-6813E15046E6';
+    usuarioId = '';
 }
 export interface PagosResponse {
     pagoId: string;
@@ -19,7 +18,6 @@ export interface PagosResponse {
     fechaVencimiento: Date;
     monto: number;
     usuarioId: number;
-    tratamientoId: string; 
 }
 export interface PagosListData {
     data: PagosResponse[];
