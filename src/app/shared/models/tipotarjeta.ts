03af71a81fea083c3b351553e198132d7f9c068f
JASIIEL <130499497+JASIIEL@users.noreply.github.com>
2023-12-14 18:11:33 -0500

    uptade - tipo tarjeta

--- src/app/shared/models/tipotarjeta.ts
+++ src/app/shared/models/tipotarjeta.ts
@@ -3,12 +3,12 @@ export class tipoTarjeta {
     usuarioId = "";
     tipoTarjetaId = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
     descripcion= "";
-    estado!: number;;
+    estado!: number;
 }
 export interface ITipoTarjeta {
     tipoTarjetaId: string;
     descripcion: string;
-    estado: number;
+    estado: string;
 }
 export interface DataTipoTarjetas {
     totalData: number;
