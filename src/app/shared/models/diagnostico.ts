ded1548849e9ef3d0d8be95c9692aa397f3f4ce8
HorDdaTh <nicolasalf147@gmail.com>
2023-12-20 12:03:11 -0500

    update | estado

--- src/app/shared/models/diagnostico.ts
+++ src/app/shared/models/diagnostico.ts
@@ -2,7 +2,7 @@ export class diagnostico {
     clinicaId = 'D30C2D1E-E883-4B2D-818A-6813E15046E6';
     usuarioId = "";
     codigoEnfermedad = "";
-    estado="";
+    
   }
   export interface DataDiagnostico {
     totalData: number;
