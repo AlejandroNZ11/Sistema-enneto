6a7f0f7d6998f91706c82759ff519eabbd2c9fbf
Cristhofer Salcedo <emoword123@gmail.com>
2024-01-15 16:17:31 -0500

    Merge branch 'dev' of https://github.com/RiLaRos/dental-front into dev

--- src/app/core/patient/historia-paciente/historia-paciente.component.ts
+++ src/app/core/patient/historia-paciente/historia-paciente.component.ts
@@ -1,17 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { SharedService } from './services/shared-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-historia-paciente',
   templateUrl: 'historia-paciente.component.html'
 })
 
-export class HistoriaPacienteComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+export class HistoriaPacienteComponent {
+
   pacienteId = "";
 
-  ngOnInit() {
+  base='' //Inicializar la base Url
+  terminosBuscados:string[] = ['historia-clinica', 'odontograma', 'filiacion','ortodoncia','presupuesto','diagnostico','evolucion','imagenes','estado-cuenta','consentimiento','recetas'];
 
+  constructor(private route: ActivatedRoute, private router: Router, private sharedService: SharedService) {
+
+    router.events.subscribe((event: object) => {
+      if (event instanceof NavigationEnd) {
+        this.getRoutes(event);
+      }
+    });
+    this.getRoutes(this.router);
+
+   }
+
+
+  private getRoutes(route: { url: string }): void {
+
+    const splitVal = route.url.split('/');
+    // Buscar si incluye el término en la Url
+    const terminoEncontrado = this.terminosBuscados.find(termino => splitVal.includes(termino));
+
+    if (terminoEncontrado) {
+        this.base = terminoEncontrado;
+    } else {
+        this.base = '';
+    }
 
   }
+
 }
--- src/app/core/patient/historia-paciente/mini-sidebar/mini-sidebar.component.ts
+++ src/app/core/patient/historia-paciente/mini-sidebar/mini-sidebar.component.ts
@@ -1,28 +1,94 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { routes } from 'src/app/shared/routes/routes';
 import { SharedService } from '../services/shared-service.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { PacienteService } from 'src/app/shared/services/paciente.service';
+import { PacienteEditar } from 'src/app/shared/models/paciente';
 
 @Component({
   selector: 'app-mini-sidebar',
   templateUrl: './mini-sidebar.component.html',
   styleUrls: ['./mini-sidebar.component.scss']
 })
-export class MiniSidebarComponent {
+export class MiniSidebarComponent implements OnInit, OnDestroy{
+
+
+
 
 public routes = routes;
   activeLink='HistoriaContenidoFiliacion'
   pacienteId: string = ''; // Inicializar con algún valor predeterminado
 
-  constructor(private sharedService: SharedService) {
+  base='' //Inicializar la base Url
+  terminosBuscados:string[] = ['historia-clinica', 'odontograma', 'filiacion','ortodoncia','presupuesto','diagnostico','evolucion','imagenes','estado-cuenta','consentimiento','recetas'];
+
+  constructor(private sharedService: SharedService, private router: Router, public pacienteService: PacienteService) {
     this.sharedService.pacienteId$.subscribe(pacienteId => {
       this.pacienteId = pacienteId;
       console.log(pacienteId)
     });
+
+    router.events.subscribe((event: object) => {
+      if (event instanceof NavigationEnd) {
+        this.getRoutes(event);
+      }
+    });
+    this.getRoutes(this.router);
   }
 
   setActiveLink(link: string): void {
     this.activeLink = link;
 
+  }
+
+  private getRoutes(route: { url: string }): void {
+
+    const splitVal = route.url.split('/');
+    // Buscar si incluye el término en la Url
+    const terminoEncontrado = this.terminosBuscados.find(termino => splitVal.includes(termino));
+
+    if (terminoEncontrado) {
+        this.base = terminoEncontrado;
+    } else {
+        this.base = '';
+    }
+
+  }
+
+
+  pacienteIdSubscription!: Subscription;
+  pacienteNameSubscription!: Subscription;
+  idPaciente!:string;
+  nombres?:string;
+  apellidos?:string;
+
+  ngOnInit() {
+
+    this.pacienteIdSubscription = this.sharedService.pacienteId$.subscribe((id: string) => {
+      this.idPaciente = id;
+      if(this.idPaciente){
+        this.pacienteNameSubscription= this.pacienteService.obtenerPaciente(this.idPaciente)
+      .subscribe(async (paciente: PacienteEditar) => {
+        if (paciente) {
+          this.nombres = paciente.nombres;
+          this.apellidos = paciente.apellidos
+        }
+      })
+      }
+
+    });
+
+
+
+  }
+
+  ngOnDestroy() {
+    // Asegúrate de desuscribirte para evitar pérdidas de memoria
+    if (this.pacienteIdSubscription) {
+      this.pacienteIdSubscription.unsubscribe();
+      this.pacienteNameSubscription.unsubscribe();
+    }
   }
 
 }
