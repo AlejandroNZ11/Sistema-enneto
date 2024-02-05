1593703a5c73e6321d6c401d5b0907bac15471c1
HorDdaTh <nicolasalf147@gmail.com>
2024-02-05 13:46:42 -0500

    update | proveedor editar

--- src/app/core/operaciones/proveedor/proveedor.component.ts
+++ src/app/core/operaciones/proveedor/proveedor.component.ts
@@ -11,6 +11,7 @@ import Swal from 'sweetalert2';
 import { environment as env } from 'src/environments/environments';
 import { Accion, PageSize, Paginacion, getEntityPropiedades } from 'src/app/shared/models/tabla-columna';
 import { EditarProveedorComponent } from './editar-proveedor/editar-proveedor.component';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-proveedor',
   templateUrl: './proveedor.component.html',
@@ -109,11 +110,18 @@ export class ProveedorComponent {
   }
   editarProveedor(proveedor: Iproveedor) {
     const initialState = {
-      proveedorSeleccionado: proveedor.ruc
+      proveedorSeleccionado: proveedor.proveedorId
     };
     this.bsModalRef = this.modalService.show(EditarProveedorComponent, { initialState });
+    const proveedorActualizado = new Subject<boolean>();
+    this.bsModalRef.content.proveedorActualizado = proveedorActualizado;
+    proveedorActualizado.subscribe((proveedorEditada: boolean) => {
+      if (proveedorEditada) {
+        this.getTableData(this.currentPage, this.pageSize);
+      }
+    });
     this.bsModalRef.onHidden?.subscribe(() => {
-      this.getTableData(this.currentPage, this.pageSize);
+      proveedorActualizado.unsubscribe();   
     });
   }
   public searchData(value: any): void {
